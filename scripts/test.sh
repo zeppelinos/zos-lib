9808a6c04a36c97b11001ba9aac71f09db0e8fd7
Alejo Salles <alejosalles@gmail.com>
2018-04-10 16:46:51 -0300

    linking coverage to TRAVIS CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,9 @@
 echo "Testing... "
 if [ "$SOLIDITY_COVERAGE" = true ]; then
   node_modules/.bin/solidity-coverage
+  if [ "$CONTINUOUS_INTEGRATION" = true ]; then
+    cat coverage/lcov.info | node_modules/.bin/coveralls
+  fi
 else
   node_modules/.bin/truffle test "$@"
 fi
