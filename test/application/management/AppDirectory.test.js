366f5c378517ee286a6049c5dfb2bfa3b0a6b288
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-04-17 17:52:14 -0300

    Use stdlib instead of fallback provider

--- test/application/management/AppDirectory.test.js
+++ test/application/management/AppDirectory.test.js
@@ -1,12 +1,12 @@
+const AppDirectory = artifacts.require('AppDirectory')
 const assertRevert = require('../../helpers/assertRevert')
 const ContractDirectory = artifacts.require('ContractDirectory')
-const AppDirectory = artifacts.require('AppDirectory')
-const shouldBehaveLikeContractDirectory = require('./ContractDirectory.behavior')
+const shouldBehaveLikeContractDirectory = require('../versioning/ContractDirectory.behavior')
 
 contract('ContractDirectory', ([_, owner, fallbackOwner, anotherAddress, implementation_v0, implementation_v1, fallbackImplementation]) => {
   beforeEach(async function () {
     this.directory = await AppDirectory.new(0x0, { from: owner })
-    this.fallbackProvider = await ContractDirectory.new({ from: fallbackOwner })
+    this.stdlib = await ContractDirectory.new({ from: fallbackOwner })
   })
 
   shouldBehaveLikeContractDirectory(owner, anotherAddress, implementation_v0, implementation_v1)
@@ -14,7 +14,7 @@ contract('ContractDirectory', ([_, owner, fallbackOwner, anotherAddress, impleme
   describe('getImplementation', function () {
     const contract = 'ERC721'
 
-    describe('when no fallback provider was set', function () {
+    describe('when no stdlib was set', function () {
       describe('when the requested contract was registered in the directory', function () {
         beforeEach(async function () {
           await this.directory.setImplementation(contract, implementation_v0, { from: owner })
@@ -34,9 +34,9 @@ contract('ContractDirectory', ([_, owner, fallbackOwner, anotherAddress, impleme
       })
     })
 
-    describe('when a fallback provider was set', function () {
+    describe('when a stdlib was set', function () {
       beforeEach(async function () {
-        await this.directory.setFallbackProvider(this.fallbackProvider.address, { from: owner })
+        await this.directory.setStdlib(this.stdlib.address, { from: owner })
       })
 
       describe('when the requested contract was registered in the directory', function () {
@@ -44,9 +44,9 @@ contract('ContractDirectory', ([_, owner, fallbackOwner, anotherAddress, impleme
           await this.directory.setImplementation(contract, implementation_v0, { from: owner })
         })
 
-        describe('when the requested contract was registered in the fallback provider', function () {
+        describe('when the requested contract was registered in the stdlib', function () {
           beforeEach(async function () {
-            await this.fallbackProvider.setImplementation(contract, fallbackImplementation, { from: fallbackOwner })
+            await this.stdlib.setImplementation(contract, fallbackImplementation, { from: fallbackOwner })
           })
 
           it('returns the implementation of the directory', async function () {
@@ -55,7 +55,7 @@ contract('ContractDirectory', ([_, owner, fallbackOwner, anotherAddress, impleme
           })
         })
 
-        describe('when the requested contract was not registered in the fallback provider', function () {
+        describe('when the requested contract was not registered in the stdlib', function () {
           it('returns the implementation of the directory', async function () {
             const implementation = await this.directory.getImplementation(contract)
             assert.equal(implementation, implementation_v0)
@@ -64,18 +64,18 @@ contract('ContractDirectory', ([_, owner, fallbackOwner, anotherAddress, impleme
       })
 
       describe('when the requested contract was not registered in the directory', function () {
-        describe('when the requested contract was registered in the fallback provider', function () {
+        describe('when the requested contract was registered in the stdlib', function () {
           beforeEach(async function () {
-            await this.fallbackProvider.setImplementation(contract, fallbackImplementation, { from: fallbackOwner })
+            await this.stdlib.setImplementation(contract, fallbackImplementation, { from: fallbackOwner })
           })
 
-          it('returns the implementation of the fallback provider', async function () {
+          it('returns the implementation of the stdlib', async function () {
             const implementation = await this.directory.getImplementation(contract)
             assert.equal(implementation, fallbackImplementation)
           })
         })
 
-        describe('when the requested contract was not registered in the fallback provider', function () {
+        describe('when the requested contract was not registered in the stdlib', function () {
           it('returns the zero address', async function () {
             const implementation = await this.directory.getImplementation(contract)
             assert.equal(implementation, 0x0)
@@ -85,29 +85,29 @@ contract('ContractDirectory', ([_, owner, fallbackOwner, anotherAddress, impleme
     })
   })
 
-  describe('setFallbackProvider', function () {
+  describe('setStdlib', function () {
     describe('when the sender is the owner', function () {
       const from = owner
 
       beforeEach(async function () {
-        await this.directory.setFallbackProvider(this.fallbackProvider.address, { from })
+        await this.directory.setStdlib(this.stdlib.address, { from })
       })
 
-      it('a fallback provider can be set', async function () {
-        const fallbackProvider = await this.directory.fallbackProvider()
-        assert.equal(fallbackProvider, this.fallbackProvider.address)
+      it('a stdlib can be set', async function () {
+        const stdlib = await this.directory.stdlib()
+        assert.equal(stdlib, this.stdlib.address)
       })
 
-      it('a fallback provider can be unset', async function () {
-        await this.directory.setFallbackProvider(0, { from })
-        const fallbackProvider = await this.directory.fallbackProvider()
-        assert.equal(fallbackProvider, 0x0)
+      it('a stdlib can be unset', async function () {
+        await this.directory.setStdlib(0, { from })
+        const stdlib = await this.directory.stdlib()
+        assert.equal(stdlib, 0x0)
       })
     })
 
     describe('when the sender is not the owner', function () {
       it('reverts', async function () {
-        await assertRevert(this.directory.setFallbackProvider(this.fallbackProvider.address, { from: anotherAddress }))
+        await assertRevert(this.directory.setStdlib(this.stdlib.address, { from: anotherAddress }))
       })
     })
   })
