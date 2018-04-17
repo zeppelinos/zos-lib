f08b3e50e95ac98cd68c7d706fbc49b4f29fe45a
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-04-17 10:30:29 -0300

    Fix implementation provider mock to extend from contract provider

--- contracts/mocks/ImplementationProviderMock.sol
+++ contracts/mocks/ImplementationProviderMock.sol
@@ -1,19 +1,19 @@
 pragma solidity ^0.4.21;
 
-import '../ImplementationProvider.sol';
+import '../application/versioning/ContractProvider.sol';
 
 /**
  * @title ImplementationProviderMock
  * @dev This contract is a mock to test upgradeability functionality
  */
-contract ImplementationProviderMock is ImplementationProvider {
+contract ImplementationProviderMock is ContractProvider {
   address public implementation;
 
   function ImplementationProviderMock(address _implementation) public {
     implementation = _implementation;
   }
 
-  function getImplementation(string /*version*/, string /*contractName*/) public view returns (address) {
+  function getImplementation(string /*contractName*/) public view returns (address) {
     return implementation;
   }
 }
