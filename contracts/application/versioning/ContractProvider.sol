70eb3f59fbb2c18a7a52be6903ee76400505ba03
Alejo Salles <alejosalles@gmail.com>
2018-04-19 17:26:22 -0300

    homogeneized inline doc indentation

--- contracts/application/versioning/ContractProvider.sol
+++ contracts/application/versioning/ContractProvider.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.4.21;
  * @dev Interface for contracts that can provide implementation addresses for other contracts
  */
 interface ContractProvider {
-   /**
+  /**
    * @dev prototype of function that returns implementation addresses
    * @param contractName Name of the contract whose address is being fetched
    * @return Implementation address of desired contract
