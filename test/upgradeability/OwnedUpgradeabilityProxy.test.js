fc45ac40a46f0f0d9b5a0b8c0986b164eb82eaf9
Francisco Giordano <frangio.1@gmail.com>
2018-05-09 16:57:57 -0300

    make remaining tests pass

--- test/upgradeability/OwnedUpgradeabilityProxy.test.js
+++ test/upgradeability/OwnedUpgradeabilityProxy.test.js
@@ -249,22 +249,22 @@ contract('OwnedUpgradeabilityProxy', ([_, owner, anotherAccount]) => {
       const newOwner = anotherAccount
 
       describe('when the sender is the owner', function () {
-        const from = owner
+        beforeEach('transferring', async function () {
+          const { logs } = await this.proxy.transferProxyOwnership(newOwner, { from: owner })
+          this.logs = logs
+        })
 
-        it('transfers the ownership', async function () {
-          await this.proxy.transferProxyOwnership(newOwner, { from })
-
-          const proxyOwner = await this.proxy.proxyOwner({ from: owner })
+        it('assigns new proxy owner', async function () {
+          // [TODO] maybe we should test this by reading from storage
+          const proxyOwner = await this.proxy.proxyOwner({ from: newOwner })
           assert.equal(proxyOwner, anotherAccount)
         })
 
         it('emits an event', async function () {
-          const { logs } = await this.proxy.transferProxyOwnership(newOwner, { from })
-
-          assert.equal(logs.length, 1)
-          assert.equal(logs[0].event, 'ProxyOwnershipTransferred')
-          assert.equal(logs[0].args.previousOwner, owner)
-          assert.equal(logs[0].args.newOwner, newOwner)
+          assert.equal(this.logs.length, 1)
+          assert.equal(this.logs[0].event, 'ProxyOwnershipTransferred')
+          assert.equal(this.logs[0].args.previousOwner, owner)
+          assert.equal(this.logs[0].args.newOwner, newOwner)
         })
       })
 
