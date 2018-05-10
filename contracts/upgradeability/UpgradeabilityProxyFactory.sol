b75abb235dac8eff07c590519e65fe7db9627980
Manuel Araoz <manuelaraoz@gmail.com>
2018-05-10 18:07:12 -0300

    small fixes in UpgradeabilityProxyFactory

--- contracts/upgradeability/UpgradeabilityProxyFactory.sol
+++ contracts/upgradeability/UpgradeabilityProxyFactory.sol
@@ -4,12 +4,12 @@ import './OwnedUpgradeabilityProxy.sol';
 
 /**
  * @title UpgradeabilityProxyFactory
- * @dev This contracts provides required functionality to create upgradeability proxies
+ * @dev Creates upgradeability proxies
  */
 contract UpgradeabilityProxyFactory {
   /**
-   * @dev This event will be emitted every time a new proxy is created
-   * @param proxy the address of the proxy created
+   * @dev Emitted every time a new proxy is created
+   * @param proxy The address of the created proxy
    */
   event ProxyCreated(address proxy);
 
@@ -29,7 +29,7 @@ contract UpgradeabilityProxyFactory {
    * @dev Creates an upgradeability proxy upgraded to an initial version and call the new implementation
    * @param owner the owner of the proxy to be set
    * @param implementation the address of the initial implementation to be set
-   * @param data represents the msg.data to bet sent in the low level call. This parameter may include the function
+   * @param data The msg.data to bet sent in the low level call. This parameter may include the function
    * signature of the implementation to be called with the needed payload
    * @return address of the new proxy created
    */
