c56896bf3b1f0edd0961dbc73ac4b9202b24d252
Manuel Araoz <manuelaraoz@gmail.com>
2018-05-10 17:50:08 -0300

    small fixes in OwnedUpgradeabilityProxy

--- contracts/upgradeability/OwnedUpgradeabilityProxy.sol
+++ contracts/upgradeability/OwnedUpgradeabilityProxy.sol
@@ -4,7 +4,7 @@ import './UpgradeabilityProxy.sol';
 
 /**
  * @title OwnedUpgradeabilityProxy
- * @dev This contract combines an upgradeability proxy with basic authorization control functionalities
+ * @dev OwnedUpgradeabilityProxy combines an upgradeability proxy with basic authorization control functionalities
  */
 contract OwnedUpgradeabilityProxy is UpgradeabilityProxy {
   /**
@@ -18,7 +18,7 @@ contract OwnedUpgradeabilityProxy is UpgradeabilityProxy {
   bytes32 private constant proxyOwnerSlot = keccak256("org.zeppelinos.proxy.owner");
 
   /**
-   * @dev Throws if called by any account other than the owner.
+   * @dev Throws if called by any account other than the owner
    */
   modifier onlyProxyOwner() {
     require(msg.sender == proxyOwner());
@@ -26,7 +26,7 @@ contract OwnedUpgradeabilityProxy is UpgradeabilityProxy {
   }
 
   /**
-   * @dev the constructor sets the original owner of the contract to the sender account.
+   * @dev the constructor sets the original owner of the contract to the sender account
    * @param _implementation the address of the initial implementation to be set
    */
   function OwnedUpgradeabilityProxy(address _implementation) UpgradeabilityProxy(_implementation) public {
@@ -34,7 +34,6 @@ contract OwnedUpgradeabilityProxy is UpgradeabilityProxy {
   }
 
   /**
-   * @dev Tells the address of the owner
    * @return the address of the owner
    */
   function proxyOwner() public view returns (address owner) {
@@ -45,8 +44,8 @@ contract OwnedUpgradeabilityProxy is UpgradeabilityProxy {
   }
 
   /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
+   * @dev Allows the current owner to transfer control of the contract to a newOwner
+   * @param newOwner The address to transfer ownership to
    */
   function transferProxyOwnership(address newOwner) public onlyProxyOwner {
     require(newOwner != address(0));
@@ -55,8 +54,8 @@ contract OwnedUpgradeabilityProxy is UpgradeabilityProxy {
   }
 
   /**
-   * @dev Allows the proxy owner to upgrade the current version of the proxy.
-   * @param implementation the address of the new implementation to be set.
+   * @dev Allows the proxy owner to upgrade the current version of the proxy
+   * @param implementation the address of the new implementation to be set
    */
   function upgradeTo(address implementation) public onlyProxyOwner {
     _upgradeTo(implementation);
@@ -64,8 +63,8 @@ contract OwnedUpgradeabilityProxy is UpgradeabilityProxy {
 
   /**
    * @dev Allows the proxy owner to upgrade the current version of the proxy and call the new implementation
-   * to initialize whatever is needed through a low level call.
-   * @param implementation the address of the new implementation to be set.
+   * to initialize whatever is needed through a low level call
+   * @param implementation the address of the new implementation to be set
    * @param data represents the msg.data to bet sent in the low level call. This parameter may include the function
    * signature of the implementation to be called with the needed payload
    */
