e587b31bb322cb06e28e957dd9d01d1350e6bee8
Santiago Palladino <spalladino@gmail.com>
2018-08-06 20:10:48 -0300

    New model for stdlibs

--- contracts/application/UnversionedApp.sol
+++ contracts/application/UnversionedApp.sol
@@ -10,33 +10,40 @@ import "../upgradeability/UpgradeabilityProxyFactory.sol";
  */
 contract UnversionedApp is BaseApp {
   /*
-   * @dev Provider that stores the contract implementation addresses.
+   * @dev Providers for contract implementation addresses.
    */
-  ImplementationProvider internal provider;
+  mapping(string => ImplementationProvider) internal providers;
+
+  event ProviderChanged(string providerName, address implementation);
 
   /**
    * @dev Constructor function.
-   * @param _provider Implementation provider.
    * @param _factory Proxy factory.
    */
-  constructor(ImplementationProvider _provider, UpgradeabilityProxyFactory _factory) BaseApp(_factory) public {
-    setProvider(_provider);
-  }
+  constructor(UpgradeabilityProxyFactory _factory) BaseApp(_factory) public { }
 
   /**
-   * @dev Returns the provider used by the app.
+   * @dev Returns the provider for a given package.
+   * @param packageName Name of the package to be retrieved.
    * @return The provider.
    */
-  function getProvider() internal view returns (ImplementationProvider) {
-    return provider;
+  function getProvider(string packageName) public view returns (ImplementationProvider) {
+    return providers[packageName];
   }
 
   /**
    * @dev Sets a new implementation provider.
    * @param _provider New implementation provider
    */
-  function setProvider(ImplementationProvider _provider) public onlyOwner {
+  function setProvider(string packageName, ImplementationProvider _provider) public onlyOwner {
     require(address(_provider) != address(0), "Cannot set the implementation provider of an app to the zero address");
-    provider = _provider;
+    providers[packageName] = _provider;
+    emit ProviderChanged(packageName, _provider);
+  }
+
+  function unsetProvider(string packageName) public onlyOwner {
+    require(providers[packageName] != address(0), "Provider to unset not found");
+    delete providers[packageName];
+    emit ProviderChanged(packageName, address(0));
   }
 }
