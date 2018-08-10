1d9182096c9914a6310fc535aff44707bfa88d80
Zainan Victor Zhou <zzn@zzn.im>
2018-08-09 17:32:54 -0700

    Fix compile error
    This is compile error
    ```
    ~/upgradable-contract/contracts/MyContract_v1.sol:11:9: DeclarationError: Undeclared identifier. Did you mean "_value"?
            value = _value;
            ^---^
    ~/upgradable-contract/contracts/MyContract_v1.sol:16:9: DeclarationError: Undeclared identifier. Did you mean "_value"?
            value = value + _value;
            ^---^
    ~/upgradable-contract/contracts/MyContract_v1.sol:16:17: DeclarationError: Undeclared identifier. Did you mean "_value"?
            value = value + _value;
    ```

--- examples/simple/contracts/MyContract_v1.sol
+++ examples/simple/contracts/MyContract_v1.sol
@@ -5,7 +5,7 @@ import "zos-lib/contracts/migrations/Initializable.sol";
 
 
 contract MyContract_v1 is Initializable {
-  uint256 public x;
+  uint256 public value;
   
   function initialize(uint256 _value) isInitializer public {
     value = _value;
